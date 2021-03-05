c0de83be5ab06f6011a840efdd547f594bfd05f9
jlguambo <jlguambo@gmail.com>
2021-03-05 11:36:42 -0500

    Static DDL with Select

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+
+
+
 function App() {
+  const trainee_level = [
+    {id:1,trainee_level_type:"novato"},
+    {id:2,trainee_level_type:"medio"},
+    {id:3,trainee_level_type:"avanzado"}
+  ];
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+    
+    <Select labelId="label" id="select" value="20">
+     
+      <MenuItem value="10">{trainee_level[0].trainee_level_type}</MenuItem>
+      <MenuItem value="20">{trainee_level[1].trainee_level_type}</MenuItem>
+    </Select>
+    </form>
+    
   );
 }
-
+/* 
+function App() {
+  return (
+    
+      <button type="Tipo de entrenamiento">
+        HOla
+      </button>
+ 
+  );
+}
+ */
 export default App;
