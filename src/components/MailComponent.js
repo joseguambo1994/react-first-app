1110a2067175653f33a07d37081ea70fc9503bef
jlguambo <jlguambo@gmail.com>
2021-03-06 08:48:43 -0500

    Mail Component, Values retrieved in JSON

--- src/components/MailComponent.js
+++ src/components/MailComponent.js
@@ -1,13 +1,26 @@
-import React,{useState} from 'react'
+import React,{useState,useRef} from 'react'
 
 const MailComponent = () => {
     
+
     const [state,setState] = useState(
         {firstName:"",lastName:"",email:""}
     );
-
+    var refValueFirstName = useRef();
+    var refValueLastName = useRef();
+    var refValueEmail = useRef();
+    var palabra = "";
     const clickHandler = (e)=>{
-       
+        //e.preventDefault();
+        setState({
+            firstName:refValueFirstName.current.value,
+            lastName:refValueLastName.current.value,
+            email:refValueEmail.current.value
+        });
+        console.log(refValueFirstName.current.value);
+        console.log(refValueLastName.current.value);
+        console.log(refValueEmail.current.value);
+        
     }
 
     return(
@@ -16,15 +29,17 @@ const MailComponent = () => {
                 
             
                 
-                <input type="text" name="firstName"  />
+                <input type="text" name="firstName" ref={refValueFirstName} />
             
             
                 
-                <input type="text" name="lastName" />
+                <input type="text" name="lastName" ref={refValueLastName} />
             
                 
-                <input type="email" name="email" />
-            <input type="submit" value="Submit" />
+                <input type="email" name="email" ref={refValueEmail}/>
+            <button type="button" onClick={clickHandler}>Registrar </button>
+             <p>El JSON:{JSON.stringify(state) }</p>
+             <p>La palabra ingresada es:{palabra}</p>
              </form>
  
     );
